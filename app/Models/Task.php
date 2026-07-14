00036f45f827edd8ecd71ab2cc9201387cc45ae4
hamzamostafa-git-hub <hamzamostafa5516@gmail.com>
2026-07-14 18:22:13 +0300

    updated models

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,12 +2,21 @@
 
 namespace App\Models;
 
+use App\Observers\TaskObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+#[ObservedBy([TaskObserver::class])] //ask about this
 
 class Task extends Model
 {
-    protected $fillable = [
-    'title',
-    'completed'
-];  
+    use SoftDeletes;
+
+protected $fillable = ['title', 'user_id', 'due_date'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
